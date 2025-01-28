1f2b2fa21bbd612c410c7e830efec7387cead7f4
Aman Soni <amansoni0228@gmail.com>
2025-01-29 02:53:24 +0530

    Learn

--- chai_aur_code/L13_Time_and_Date.js
+++ chai_aur_code/L13_Time_and_Date.js
@@ -3,23 +3,23 @@
 
    let myDate = new Date();
    console.log(typeof myDate);                            //*return object
-   console.log(myDate);
-   console.log(myDate.toString());
-   console.log(myDate.toDateString());
-   console.log(myDate.toISOString());
-   console.log(myDate.toJSON());
-   console.log(myDate.toLocaleString());
-   console.log(myDate.toLocaleTimeString());
-   console.log(myDate.toLocaleDateString());
+   console.log(myDate);                                   //return 2025-01-28T21:16:54.040Z
+   console.log(myDate.toString());                        //return Wed Jan 29 2025 02:46:54 GMT+0530 (India Standard Time)
+   console.log(myDate.toDateString());                    //return Wed Jan 29 2025
+   console.log(myDate.toISOString());                     //return 2025-01-28T21:16:54.040Z
+   console.log(myDate.toJSON());                          //return 2025-01-28T21:16:54.040Z
+   console.log(myDate.toLocaleString());                  //return 1/29/2025, 2:46:54 AM
+   console.log(myDate.toLocaleTimeString());              //return 2:46:54 AM
+   console.log(myDate.toLocaleDateString());              //return 1/29/2025
    
 let myCreatedDate=new Date(2025, 0, 29);                 
 console.log(myCreatedDate.toLocaleString());               //return 1/29/2025, 12;00;00 AM, in this way  month start with 0
 
 let curentDate=new Date(2025, 1, 28, 8, 5, 2);
-console.log(curentDate.toLocaleString());
+console.log(curentDate.toLocaleString());                 //return 2/28/2025, 8:05:02 AM
 
 let nowDate= new Date("01/14/2025");
-console.log(nowDate.toLocaleString());
+console.log(nowDate.toLocaleString());                     //return 1/14/2025, 12:00:00 AM
 
 // <-----Important----->
 let myTimeStamp = Date.now();
@@ -31,10 +31,10 @@ console.log(Math.floor(Date.now()/1000));                  //*return 1738097384,
 
 
 let newDate= new Date();
-console.log(newDate.getMonth());                            //return 0, which is jan, jan is current month  
-console.log(newDate.getDate());                             //return 29, which is current Date
+console.log(newDate.getMonth());                                     //return 0, which is jan, jan is current month  
+console.log(newDate.getDate());                                      //return 29, which is current Date
 
-console.log(newDate.toLocaleString());
+console.log(newDate.toLocaleString());                               //return 1/29/2025, 2:46:54 AM
 console.log(newDate.toLocaleString("default", {
          weekday: "long"                                             // click control+space to see option, and costomoze LocaleString function 
 }                                                                    //return wednesday
